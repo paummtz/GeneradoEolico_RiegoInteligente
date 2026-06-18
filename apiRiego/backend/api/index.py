d1dd5daca7d5edd00f264ea890a8a00629e5b7b4
Paulina Muñoz <160296357+paummtz@users.noreply.github.com>
2026-06-18 12:57:45 -0600

    Add files via upload

---
+++ apiRiego/backend/api/index.py
@@ -0,0 +1,6 @@
+# api/index.py
+# Importamos la variable 'app' desde tu archivo original
+from Directions import app
+
+# Vercel necesita que el objeto 'app' esté expuesto aquí.
+# Al importar 'app', ya tienes todas tus rutas configuradas.
