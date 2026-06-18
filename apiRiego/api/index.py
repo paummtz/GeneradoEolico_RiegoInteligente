80712bbc0a0d982b77098f23e4469a6a40cdf3b3
Paulina Muñoz <160296357+paummtz@users.noreply.github.com>
2026-06-18 13:17:53 -0600

    Delete apiRiego directory

--- apiRiego/api/index.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Como moviste api/ a la raíz, solo necesitas subir un nivel
-ruta_raiz = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-if ruta_raiz not in sys.path:
-    sys.path.append(ruta_raiz)
-
-from Directions import app
--- apiRiego/backend/GlobalInfo/ResponseMessages.py
+++
@@ -1,7 +0,0 @@
-err500={'intResponse':'500','strAnswer':'Server Error.'}
-
-err203={'intResponse':'203','strAnswer':'Wrong data.'}
-
-err205={'intResponse':'205','strAnswer':'Invalid Token'}
-
-succ200={'intResponse':'200','strAnswer':'Success request'}
