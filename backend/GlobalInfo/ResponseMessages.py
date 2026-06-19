1c260e8164914aed1a3f6acf4d9cbdca649fd081
Paulina Muñoz <160296357+paummtz@users.noreply.github.com>
2026-06-18 19:56:43 -0600

    Add files via upload

---
+++ backend/GlobalInfo/ResponseMessages.py
@@ -0,0 +1,7 @@
+err500={'intResponse':'500','strAnswer':'Server Error.'}
+
+err203={'intResponse':'203','strAnswer':'Wrong data.'}
+
+err205={'intResponse':'205','strAnswer':'Invalid Token'}
+
+succ200={'intResponse':'200','strAnswer':'Success request'}
