948c402dae0953ed5e3169aae568067760a08920
Paulina Muñoz <160296357+paummtz@users.noreply.github.com>
2026-06-18 19:40:55 -0600

    Delete backend directory

--- backend/GlobalInfo/Keys.py
+++
@@ -1,6 +0,0 @@
-import sys
-dbconn = None
-
-strConnection = "mongodb+srv://leonidas:kNIfYKmoPD1DUjDZ@cluster0.ttgkw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-strDBConnection = "Riego"
